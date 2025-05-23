7d82f610c16eb5fd27f6848808377acbd83c6c5a
Kosiso Abone <abonekosiso@gmail.com>
2025-05-23 18:11:28 +0000

    modified validation logic to store, track and retrieve number of failed attempts from session variable 'failed_attempts'

--- validate.php
+++ validate.php
@@ -1,16 +1,24 @@
 <?php
 
-  $valid_username = "Kosy";
-  $valid_password = "Test";
+  session_start();
+
+  $valid_username = "kosy";
+  $valid_password = "test";
 
   $username = $_REQUEST['username'];
   $password = $_REQUEST['password'];
 
   if($valid_username == $username && $valid_password == $password){
-    echo 'success';
+    header('location: /');
   }
   else{
-    echo 'fail';
+    if(!isset($_SESSION['failed_attempts'])){
+      $_SESSION['failed_attempts'] = 1;
+    }
+    else{
+      $_SESSION['failed_attempts'] += 1;
+    }
+    echo 'This is unsuccessful attempt number ' . $_SESSION['failed_attempts'];
   }
 
 ?>
